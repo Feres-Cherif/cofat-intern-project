bc1797bed20967e1ba39b056e09def1c57a0eed8
Feres cherif <cherifferes123@gmail.com>
2025-08-28 09:33:12 +0100

    complet hero section and footer

--- cofat-playground/src/app/app.ts
+++ cofat-playground/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Navbar } from './components/Home/navbar/navbar';
 import { Section } from "./components/Home/section/section";
+import { Footer } from "./components/Home/footer/footer";
 
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Section],
+  imports: [Navbar, Section, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
